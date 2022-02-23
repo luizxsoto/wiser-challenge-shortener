012fc5167c9a8285aad7056d3523e4b139a16f33
luizxsoto <luizxsoto@gmail.com>
2022-02-23 15:00:49 -0300

    feat: call external api

--- app/Modules/Tests/Infra/Http/Controllers/Test.ts
+++ app/Modules/Tests/Infra/Http/Controllers/Test.ts
@@ -1,4 +1,5 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import axios from 'axios'
 
 class TestController {
   public async list({ request, response }: HttpContextContract): Promise<void | { success: true }> {
@@ -9,15 +10,48 @@ class TestController {
     return response.status(200).send({ success: true })
   }
 
-  public async store({ request, response }: HttpContextContract): Promise<void | { success: true }> {
-    const requestData = request.all()
+  public async store({
+    request,
+    response,
+  }: HttpContextContract): Promise<void | { success: true }> {
+    const requestBody = request.body()
+    const requestQuery = request.qs()
+    const requestParams = request.params()
+    const { enterpriseSlug, organizationSlug, enterpriseId } = requestQuery
+    const { microservice, origin } = requestParams
 
-    console.log('TestController.store', requestData)
+    console.log('TestController.store', { requestBody, requestQuery, requestParams })
+
+    const httpClient = axios.create({ baseURL: 'https://dev-api.mercurypay.io/v1' })
+
+    const result = await httpClient
+      .request({
+        method: 'post',
+        url: `/webhooks/${microservice}/${origin}`,
+        data: requestBody,
+        params: {
+          enterpriseSlug,
+          organizationSlug,
+          enterpriseId,
+        },
+      })
+      .catch((error) =>
+        console.error(
+          'TestController.store',
+          'error',
+          error?.response?.data || error?.response || error
+        )
+      )
+
+    console.info('TestController.store', 'result', result?.data)
 
     return response.status(200).send({ success: true })
   }
 
-  public async update({ request, response }: HttpContextContract): Promise<void | { success: true }> {
+  public async update({
+    request,
+    response,
+  }: HttpContextContract): Promise<void | { success: true }> {
     const requestData = request.all()
 
     console.log('TestController.update', requestData)
@@ -25,7 +59,10 @@ class TestController {
     return response.status(200).send({ success: true })
   }
 
-  public async destroy({ request, response }: HttpContextContract): Promise<void | { success: true }> {
+  public async destroy({
+    request,
+    response,
+  }: HttpContextContract): Promise<void | { success: true }> {
     const requestData = request.all()
 
     console.log('TestController.destroy', requestData)
--- app/Modules/Tests/Infra/Http/Routes/Test.routes.ts
+++ app/Modules/Tests/Infra/Http/Routes/Test.routes.ts
@@ -3,6 +3,10 @@ import Route from '@ioc:Adonis/Core/Route'
 const ROOT_DIR = '../../..'
 
 Route.get('tests', `${ROOT_DIR}/app/Modules/Tests/Infra/Http/Controllers/Test.list`)
+Route.post(
+  'webhooks/:microservice/:origin',
+  `${ROOT_DIR}/app/Modules/Tests/Infra/Http/Controllers/Test.store`
+)
 Route.post('tests', `${ROOT_DIR}/app/Modules/Tests/Infra/Http/Controllers/Test.store`)
 Route.put('tests', `${ROOT_DIR}/app/Modules/Tests/Infra/Http/Controllers/Test.update`)
 Route.delete('tests', `${ROOT_DIR}/app/Modules/Tests/Infra/Http/Controllers/Test.destroy`)
